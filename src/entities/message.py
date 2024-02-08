fe3a39d6314ef00adfd113a53f23c39294e17a8f
Pedro <55058149+PedroG022@users.noreply.github.com>
2024-02-08 20:24:07 -0300

    🎨 Updated fields on message

--- src/entities/message.py
+++ src/entities/message.py
@@ -1,3 +1,7 @@
+from src.entities import ClientIdentifier
+
+
 class Message:
-    def __init__(self, body, sender, recipient):
-        pass
+    def __init__(self, body: str, identifier: ClientIdentifier = None):
+        self.body: str = body
+        self.identifier: ClientIdentifier = identifier
