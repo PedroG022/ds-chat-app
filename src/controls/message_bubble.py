bf08e2eaa70e85deb37ea9350425034c4d5666a9
Pedro <55058149+PedroG022@users.noreply.github.com>
2024-02-13 10:11:01 -0300

    ✨ Add bubble ownership verification and style

--- src/controls/message_bubble.py
+++ src/controls/message_bubble.py
@@ -1,28 +1,63 @@
 import flet as ft
 
+from src.entities import Message
+
 
 class MessageBubble(ft.UserControl):
-    def __init__(self, message: str, sender: str):
+    def __init__(self, message: Message, self_identifier: str):
         super().__init__()
 
-        self.__message = message
-        self.__sender = sender
+        self.__message: Message = message
+        self.__owner = self.__message.identifier == self_identifier
 
     def build(self):
-        bubble = ft.Container(
-            bgcolor=ft.colors.BLUE_700,
-            border_radius=10,
-            padding=8,
-            content=ft.Column(
-                spacing=0,
-                controls=[
-                    ft.Text(f'{self.__sender}', size=12, weight=ft.FontWeight.W_600),
-                    ft.Container(
-                        ft.Text(self.__message),
-                        margin=ft.margin.only(0, 8, 0, 0)
-                    )
-                ]
-            )
+        is_server = self.__message.identifier and self.__message.identifier.client_id == 'SERVER'
+        bubble_color = ft.colors.BLUE_700
+
+        if self.__owner:
+            bubble_color = ft.colors.GREEN_500
+
+        if is_server:
+            bubble_color = ft.colors.GREY_800
+
+        sender_title = None
+
+        if self.__message.identifier:
+            sender_title = ft.Text(f'{self.__message.identifier.name}', size=12, weight=ft.FontWeight.W_600)
+
+        message_content = ft.Text(self.__message.body)
+
+        margin_top = 8 if not (self.__owner or is_server) else 0
+
+        holder_column = ft.Column(
+            alignment=ft.MainAxisAlignment.CENTER,
+            spacing=0,
         )
 
-        return bubble
+        if not self.__owner and self.__message.identifier and not is_server:
+            holder_column.controls.append(sender_title)
+
+        holder_column.controls.append(ft.Container(
+            margin=ft.margin.only(0, margin_top, 0, 0),
+            content=message_content
+        ))
+
+        bubble = ft.Container(
+            bgcolor=bubble_color,
+            border_radius=10,
+            padding=8,
+            content=holder_column
+        )
+
+        alignment = None
+
+        if self.__owner:
+            alignment = ft.MainAxisAlignment.END
+
+        if is_server:
+            alignment = ft.MainAxisAlignment.CENTER
+
+        return ft.Row(
+            [bubble],
+            alignment=alignment,
+        )
